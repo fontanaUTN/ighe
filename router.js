e69d38b0f4741cb35f7d1028a830a96f42700043
fontanaUTN <fontana.utn@gmail.com>
2025-10-08 11:46:28 -0300

    add page home in raiz

--- router.js
+++ router.js
@@ -2,11 +2,10 @@ import express from 'express';
 const router = express.Router();
 
 import cteIndex from './controllers/clients/index.js';
-router.get('/', cteIndex.home );
+router.get('/', (req, res) => { res.render('clients/home') } );
 router.get('/servicios/Laboratorio_de_Geotecnia', (req, res) => { res.render('clients/laboratorio') });
 router.get('/servicios/Ingenieria_y_Consultoria', (req, res) => { res.render('clients/consultora') });
 router.get('/servicios/Innovacion_y_Desarrollo', (req, res) => { res.render('clients/innovacion') });
-router.get('/home', (req, res) => { res.render('clients/home') } );
 router.post('/contact/message', cteIndex.message );
 
 export default router;
